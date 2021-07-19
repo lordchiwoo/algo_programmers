82adf456574faf8273c70cc91cd1feeac1a4ea77
Leone.Kim <leone@tourmktg.co.kr>
2021-07-19 16:44:05 +0900

    로테이션을 틀려서 헤멤..ㅠ 멍충이

--- src/ILockAndKey/Solution.java
+++ src/ILockAndKey/Solution.java
@@ -33,127 +33,169 @@ public class Solution {
         int lockSize = lock.length;
         Lock myLock = new Lock(lock);
 
-        // 이동 범위 : lockSize + 2*(keySize-1)
-        int offsetSize = keySize-1;
-        int minOffset = -1 * offsetSize;
-        int maxOffset = lockSize; //  + offsetSize
-        for(int rotation=0; rotation<=270; rotation +=90)
-        {
-            //로테이션 후의 키 생성 
+        // key 0,0 좌표의 이동 가능 범위 : (-1*(keySize-1)) ~ (lockSize-1)
+        int minOffset = 1 - keySize; // -1 * keySize-1;
+        int maxOffset = lockSize - 1;
+        for (int rotation = 0; rotation <= 270; rotation += 90) {
+            // 로테이션 후의 키 생성
             Key newKey = new Key(key, rotation);
-            boolean keyMatching=true;
-            for(int offsetH = minOffset ; offsetH < maxOffset ; offsetH++)
-            {
-                for(int offsetV = minOffset ; offsetV < maxOffset ; offsetV++)
-                { 
-                    System.out.println("["+offsetH + ", " + offsetV + "] R" +rotation);
-                    if(offsetH==0 && offsetV==1 && rotation==180) 
+            boolean keyMatching = true;
+            for (int offsetH = minOffset; offsetH <= maxOffset; offsetH++) {
+                for (int offsetV = minOffset; offsetV <= maxOffset; offsetV++) {
+
+                    System.out.println("[" + offsetH + ", " + offsetV + "] R" + rotation);
+                    if (offsetH == 0 && offsetV == 1 && rotation == 180)
                         keyMatching = false;
-                    keyMatching = true;
+                    keyMatching = isMatchKey(newKey, myLock, offsetH, offsetV);
 
-                    for(int y=0; y < lockSize ; y ++)
-                    {
-                        for(int x=0; x < lockSize ; x ++)
-                        {
+                    if (keyMatching) {
 
-                            int keyStoneX = x-offsetH;                                
-                            int keyStoneY = y-offsetV;        
-
-                            int keyStone = newKey.getStoneOnXY(keyStoneX, keyStoneY);
-                            int lockStone = myLock.getStoneOnXY(x, y);
-                            
-                            System.out.print( keyStone +"/" +lockStone +" " );
-                            if(1 != keyStone+lockStone)
-                            {
-                                keyMatching=false;
-                                
-                            }
-                        }
-                        System.out.println(" ");
-                        
-                    }   
-                    System.out.println(" ");
-                    if(keyMatching){
-                        
                         return true;
-                    } 
+                    }
                 }
             }
         }
-            
+
         // 검사 범위 :
 
         return false;
     }
 
-    class Lock{
-        int[][] data;
-        Lock(int[][] lock){
-            data = lock;
+    private boolean isMatchKey(Key newKey, Lock myLock, int offsetH, int offsetV) {
+        boolean matched = true;
+        for (int y = 0; y < myLock.size; y++) {
+            for (int x = 0; x < myLock.size; x++) {
+                int keyStoneX = x - offsetH;
+                int keyStoneY = y - offsetV;
+
+                int keyStone = newKey.getXY(keyStoneX, keyStoneY);
+                int lockStone = myLock.getXY(x, y);
+
+                // System.out.print( keyStone +"/" +lockStone +" " );
+                if (1 != keyStone + lockStone) {
+                    matched = false;
+
+                }
+            }
+            // System.out.println(" ");
+
         }
-        int getStoneOnXY(int x, int y) {
+        return matched;
+    }
+
+    class Arr2d {
+        int[][] data;
+        int size;
+
+        Arr2d() {
+            data = null;
+            size = 0;
+        }
+
+        Arr2d(int[][] orgData) {
+            data = orgData;
+            size = data.length;
+        }
+
+        int getXY(int x, int y) {
+            if (x < 0 || x >= size)
+                return 0;
+            if (y < 0 || y >= size)
+                return 0;
+
             return data[x][y];
         }
     }
-    class Key {
+
+    class Lock extends Arr2d {
+        Lock(int[][] orgData) {
+            super(orgData);
+        }
+    }
+
+    class Key extends Arr2d {
         int[][] rawData;
-        int[][] data;
-        int keySize;
+
+        Key(int[][] orgData) {
+            super(orgData);
+        }
 
         Key(int[][] key, int rotation) {
             rawData = key;
             data = new int[rawData.length][rawData[0].length];
 
-            keySize = data.length;
-            for(int x=0; x < keySize; x++)
-                for(int y=0; y < keySize; y++)
+            size = data.length;
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
                     data[x][y] = getXYR(x, y, rotation);
         }
-//, int offsetH, int offsetV
-        int getXYR(int x, int y, int rotation ) {
-            int temp=0;
+
+        // , int offsetH, int offsetV
+        int getXYR(int x, int y, int rotation) {
+            int temp = 0;
             switch (rotation) {
                 case 0:
                     break;
                 case 90:
                     temp = x;
-                    x=keySize-y-1;
-                    y=temp;
+                    x = size - y - 1;
+                    y = temp;
                     break;
                 case 180:
-                    x=keySize-x-1;
-                    y=keySize-y-1;
+                    x = size - x - 1;
+                    y = size - y - 1;
                     break;
                 case 270:
-                    temp = x;
-                    y=keySize-x-1;
-                    x=temp;
+                    temp = y;
+                    y = size - x - 1;
+                    x = temp;
                     break;
                 default:
 
             }
-            /*
-            x=x+offsetH;
-            y=y+offsetV;
 
-            */
-            //Nullable?
             return rawData[x][y];
         }
-        //
-        int getStoneOnXY(int x, int y) {
-            if(x<0 || x>=keySize)
-                return 0;
-            if(y<0 || y>=keySize)
-                return 0;
-
-            return data[x][y];
-        }
-
     }
 }
 
 /*
-
-
-*/
+테스트 1 〉	통과 (20.39ms, 53.5MB)
+테스트 2 〉	통과 (15.42ms, 52.8MB)
+테스트 3 〉	통과 (35.08ms, 53MB)
+테스트 4 〉	통과 (16.06ms, 52.7MB)
+테스트 5 〉	통과 (52.48ms, 53MB)
+테스트 6 〉	통과 (51.38ms, 54.8MB)
+테스트 7 〉	통과 (43.81ms, 52.5MB)
+테스트 8 〉	통과 (78.42ms, 53.9MB)
+테스트 9 〉	통과 (88.77ms, 53.9MB)
+테스트 10 〉	통과 (103.56ms, 53.9MB)
+테스트 11 〉	통과 (110.52ms, 53.9MB)
+테스트 12 〉	통과 (13.90ms, 52.3MB)
+테스트 13 〉	통과 (28.80ms, 53.3MB)
+테스트 14 〉	통과 (23.86ms, 53.8MB)
+테스트 15 〉	통과 (28.16ms, 53MB)
+테스트 16 〉	통과 (75.85ms, 53.6MB)
+테스트 17 〉	통과 (15.83ms, 53MB)
+테스트 18 〉	통과 (54.34ms, 54.7MB)
+테스트 19 〉	통과 (19.19ms, 52.6MB)
+테스트 20 〉	통과 (118.18ms, 53.4MB)
+테스트 21 〉	통과 (35.37ms, 53.4MB)
+테스트 22 〉	통과 (48.90ms, 53.2MB)
+테스트 23 〉	통과 (35.16ms, 53MB)
+테스트 24 〉	통과 (28.40ms, 52.4MB)
+테스트 25 〉	통과 (109.93ms, 53.3MB)
+테스트 26 〉	통과 (94.26ms, 54.5MB)
+테스트 27 〉	통과 (125.37ms, 53.8MB)
+테스트 28 〉	통과 (43.30ms, 53.4MB)
+테스트 29 〉	통과 (34.94ms, 53MB)
+테스트 30 〉	통과 (53.68ms, 52.3MB)
+테스트 31 〉	통과 (95.94ms, 53.7MB)
+테스트 32 〉	통과 (105.64ms, 54.4MB)
+테스트 33 〉	통과 (46.71ms, 54MB)
+테스트 34 〉	통과 (18.60ms, 53.6MB)
+테스트 35 〉	통과 (30.99ms, 53.9MB)
+테스트 36 〉	통과 (30.83ms, 52.7MB)
+테스트 37 〉	통과 (26.77ms, 52.7MB)
+테스트 38 〉	통과 (20.50ms, 52.7MB)
+ */
